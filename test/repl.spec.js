77b930dbf7f5dc581c21ff663abbbba13d314087
hoeck <eriksoehnel@googlemail.com>
2017-01-01 13:22:39 +0100

    Really simple nrepl test
    Most of the nrepl logic is buried in the generated build.clj, which is not
    in the scope of this test.

--- test/repl.spec.js
+++ test/repl.spec.js
@@ -1,6 +1,6 @@
 const {scriptEnv, cljsbuild, mockMavenAndClojars, resetScriptEnv} = require('./test');
 
-describe('the repl command', () => {
+describe('the repl and nrepl commands', () => {
     let mvnSpy;
     let mvnSpyClasspath;
     let javaSpy;
@@ -44,73 +44,90 @@ describe('the repl command', () => {
         javaSpy = scriptEnv.mockCommand('java');
     });
 
-    it('should spin up a java process running the repl', function * () {
+    describe('repl', () => {
+        it('should spin up a java process running the repl', function * () {
 
-        yield cljsbuild('repl');
+            yield cljsbuild('repl');
 
-        expect(mvnSpy).toHaveBeenCalledWith({args: mvnArgs});
+            expect(mvnSpy).toHaveBeenCalledWith({args: mvnArgs});
 
-        // java is called two times: to build cljs and to start the repl
-        expect(javaSpy.calls.allArgs()).toEqual([
-            [{args: getJavaArgs('foo-bar-classpath')}],
-            [{args: getJavaArgs('foo-bar-classpath')}]
-        ]);
-    });
-
-    it('should use rlwrap if available', function * () {
-        const rlwrapSpy = scriptEnv.mockCommand('rlwrap');
-
-        yield cljsbuild('repl');
-
-        expect(rlwrapSpy.calls.allArgs()).toEqual([
-            // first call to detect that rlwrap is available
-            [{args: ['-v']}],
-            // second call for the repl
-            [{args: ['java'].concat(getJavaArgs('foo-bar-classpath'))}]
-        ]);
-    });
-
-    it('should use a cached classpath file if depdendencies are unchanged', function * () {
-        // first call caches the classpath
-        yield cljsbuild('repl');
-
-        expect(mvnSpy).toHaveBeenCalled();
-        expect(scriptEnv.readFiles()).toEqual(jasmine.objectContaining({
-            '.cljsbuild/classpath.hash': '3e6dc8fd0c699061d6bb7d1ba26ccb29831b4cbf',
-            '.cljsbuild/classpath.value': 'foo-bar-classpath'
-        }));
-
-        // the second call uses the cached classpath
-        mvnSpy.calls.reset();
-        javaSpy.calls.reset();
-
-        yield cljsbuild('repl');
-
-        expect(mvnSpy).not.toHaveBeenCalled();
-        expect(javaSpy).toHaveBeenCalledWith({args: getJavaArgs('foo-bar-classpath')});
-
-        // changing the dependencies should update the cache
-        mvnSpy.calls.reset();
-        mvnSpyClasspath = 'foo-bar-updated-classpath';
-        javaSpy.calls.reset();
-        scriptEnv.writeFiles({
-            'package.json': JSON.stringify({
-                cljsbuild: {
-                    main: 'foo-main',
-                    dependencies: {
-                        'com.foo/bar': '1.2.4'
-                    }
-                }
-            })
+            // java is called two times: to build cljs and to start the repl
+            expect(javaSpy.calls.allArgs()).toEqual([
+                [{args: getJavaArgs('foo-bar-classpath')}],
+                [{args: getJavaArgs('foo-bar-classpath')}]
+            ]);
         });
 
-        yield cljsbuild('repl');
+        it('should use rlwrap if available', function * () {
+            const rlwrapSpy = scriptEnv.mockCommand('rlwrap');
 
-        expect(mvnSpy).toHaveBeenCalledWith({args: mvnArgs});
-        expect(javaSpy).toHaveBeenCalledWith({args: getJavaArgs('foo-bar-updated-classpath')});
-        expect(scriptEnv.readFiles()).toEqual(jasmine.objectContaining({
-            '.cljsbuild/classpath.hash': 'fde3520603de14df16c1495b78816c7bf200d9dd',
-            '.cljsbuild/classpath.value': 'foo-bar-updated-classpath'
-        }));
+            yield cljsbuild('repl');
+
+            expect(rlwrapSpy.calls.allArgs()).toEqual([
+                // first call to detect that rlwrap is available
+                [{args: ['-v']}],
+                // second call for the repl
+                [{args: ['java'].concat(getJavaArgs('foo-bar-classpath'))}]
+            ]);
+        });
+
+        it('should use a cached classpath file if depdendencies are unchanged', function * () {
+            // first call caches the classpath
+            yield cljsbuild('repl');
+
+            expect(mvnSpy).toHaveBeenCalled();
+            expect(scriptEnv.readFiles()).toEqual(jasmine.objectContaining({
+                '.cljsbuild/classpath.hash': '3e6dc8fd0c699061d6bb7d1ba26ccb29831b4cbf',
+                '.cljsbuild/classpath.value': 'foo-bar-classpath'
+            }));
+
+            // the second call uses the cached classpath
+            mvnSpy.calls.reset();
+            javaSpy.calls.reset();
+
+            yield cljsbuild('repl');
+
+            expect(mvnSpy).not.toHaveBeenCalled();
+            expect(javaSpy).toHaveBeenCalledWith({args: getJavaArgs('foo-bar-classpath')});
+
+            // changing the dependencies should update the cache
+            mvnSpy.calls.reset();
+            mvnSpyClasspath = 'foo-bar-updated-classpath';
+            javaSpy.calls.reset();
+            scriptEnv.writeFiles({
+                'package.json': JSON.stringify({
+                    cljsbuild: {
+                        main: 'foo-main',
+                        dependencies: {
+                            'com.foo/bar': '1.2.4'
+                        }
+                    }
+                })
+            });
+
+            yield cljsbuild('repl');
+
+            expect(mvnSpy).toHaveBeenCalledWith({args: mvnArgs});
+            expect(javaSpy).toHaveBeenCalledWith({args: getJavaArgs('foo-bar-updated-classpath')});
+            expect(scriptEnv.readFiles()).toEqual(jasmine.objectContaining({
+                '.cljsbuild/classpath.hash': 'fde3520603de14df16c1495b78816c7bf200d9dd',
+                '.cljsbuild/classpath.value': 'foo-bar-updated-classpath'
+            }));
+        });
+    });
+
+    describe('nrepl', () => {
+        it('should spin up a java process running the repl server', function * () {
+
+            yield cljsbuild('nrepl');
+
+            expect(mvnSpy).toHaveBeenCalledWith({args: mvnArgs});
+
+            // java is called two times: to build cljs and to start the nrepl server
+            expect(javaSpy.calls.allArgs()).toEqual([
+                [{args: getJavaArgs('foo-bar-classpath')}],
+                [{args: getJavaArgs('foo-bar-classpath')}]
+            ]);
+        });
     });
 });
